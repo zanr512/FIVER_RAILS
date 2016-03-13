d98bd503d7c83a37b8ee05515332fcb993a00be6
zigaprasnic <zigaprasnic@gmail.com>
2016-03-13 16:10:05 +0000

    bootstrap

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,7 @@ Rails.application.configure do
   config.cache_classes = true
   config.serve_static_assets = true
   config.assets.compile = true
-  config.serve_static_files = true 
-  config.assets.initialize_on_precompile = true
+  config.serve_static_files = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
