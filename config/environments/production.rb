962d0975aa6012a9866d2c5ef50ca9c62ad9e6ed
zigaprasnic <zigaprasnic@gmail.com>
2016-03-13 18:32:48 +0000

    javascript

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,6 @@ Rails.application.configure do
   config.serve_static_assets = true
   config.assets.compile = true
   config.serve_static_files = true
-  config.assets.precompile += %w( *.js *.css *.eot *.woff *.ttf *.svg )
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
